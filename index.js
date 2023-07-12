43a60af6143e75d5b791f1d349eacff85dd25943
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2023-07-12 17:34:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var loader=document.querySelector(".preloader");
+window.addEventListener('click',function(){
+    loader.style.display="none";
+   
+});
+
+const stars=document.querySelectorAll('#star');
+
+stars.forEach(star=>{
+    let duration=Math.random()*(1.2-0.6)+0.6;
+    star.style.animation=`stars ${duration}s  infinite linear`;
+});
